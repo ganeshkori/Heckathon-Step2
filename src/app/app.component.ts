3fcfe7dcdceebedff229ff3f08b50ae13e57bc16
ganeshkori <ganesh@pragiti.com>
2018-02-07 09:18:51 +0530

    Removed extra code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,13 +27,13 @@ export class AppComponent implements OnInit {
 
   addRowColumn() {
     for (this.rowNumber = 1; this.rowCount >= this.rowNumber; this.rowNumber++) {
-      console.log(this.rowNumber);
+      //console.log(this.rowNumber);
       this.rowLoopData.push(this.rowNumber);
 
     }
 
     for (this.columnNumber = 1; this.columnCount >= this.columnNumber; this.columnNumber++) {
-      console.log(this.columnNumber);
+      //console.log(this.columnNumber);
       this.columnLoopData.push(this.columnNumber);
 
     }
@@ -47,49 +47,4 @@ export class AppComponent implements OnInit {
     this.addRowColumn();
     this.command = true;
   }
-
-  numberOfElementsToRemove = 0;
-  rowColumnCommandForm(formData: NgForm){
-    //direction
-    //indicator
-    //index
-
-    var startIndex = 0;
-    this.numberOfElementsToRemove = 0;
-
-    if (formData.value.indicator === 1) {
-      // for column
-      if (formData.value.direction > 0){
-        this.numberOfElementsToRemove = this.columnCount - formData.value.direction;
-      }
-      if (formData.value.direction < 0){
-        var numberOfElementsToRemove0 = this.columnCount + formData.value.direction;
-        this.numberOfElementsToRemove = this.columnCount - numberOfElementsToRemove0;
-      }
-      var removedData = this.columnLoopData.splice(startIndex, this.numberOfElementsToRemove);
-      for (let getData of removedData) {
-        this.columnLoopData.push(getData);
-      }
-      this.indicatorMsg = '';
-
-    } else if (formData.value.indicator === 0) {
-      // for row
-      if(formData.value.direction > 0){
-        this.numberOfElementsToRemove = this.rowCount - formData.value.direction;
-      }
-      if(formData.value.direction < 0){
-        var numberOfElementsToRemove0 = this.rowCount + formData.value.direction;
-        this.numberOfElementsToRemove = this.rowCount - numberOfElementsToRemove0;
-        console.log("< 0");
-      }
-      var removedData = this.rowLoopData.splice(startIndex, this.numberOfElementsToRemove);
-      for(let getData of removedData) {
-        this.rowLoopData.push(getData);
-      }
-      this.indicatorMsg = "";
-    }else{
-      this.indicatorMsg = "Please enter valid number (1 for col, 0 for row)";
-    }
-  }
-
 }
